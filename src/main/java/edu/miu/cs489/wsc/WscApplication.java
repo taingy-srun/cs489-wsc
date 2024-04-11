fff3ba98673faeb0e2c4dc4235844fdb1d56f49c
taingy-srun <taingy.srun20@gmail.com>
2024-04-11 14:31:32 -0500

    add project source

---
+++ src/main/java/edu/miu/cs489/wsc/WscApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs489.wsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WscApplication.class, args);
+    }
+
+}
